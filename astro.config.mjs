c56776f282606c0e28d52d3da10c9ce360a0c3ed
Tom van Dinther <tom.vandinther@myob.com>
2023-02-10 13:26:49 +1300

    Deploy to Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import inlineSVG from '@jsdevtools/rehype-inline-svg';
-
+// import inlineSVG from '@jsdevtools/rehype-inline-svg';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +11,7 @@ export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
   markdown: {
     rehypePlugins: []
-  }
+  },
+  output: "server",
+  adapter: cloudflare({ mode: "directory" }),
 });
