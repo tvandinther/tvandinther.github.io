de0bc1705f4576586235b10b9d4569ddac241e78
Tom van Dinther <tom.vandinther@myob.com>
2023-02-11 23:00:35 +1300

    Deploy with analytics to mioi.io on Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.mioi.io",
-  integrations: [tailwind(), sitemap(), prefetch()],
+  integrations: [tailwind(), sitemap(), prefetch(), partytown()],
   markdown: {
     rehypePlugins: []
-  },
+  }
 });
