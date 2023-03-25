57e5d128b3767f2730c21609daf372a5067d596b
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2023-03-25 17:49:16 +1300

    Featured projects (#9)

---
+++ src/helpers/projects.ts
@@ -0,0 +1,7 @@
+import { CollectionEntry, getCollection } from "astro:content";
+
+export async function getAllProjects() {
+    return await getCollection('projects')
+}
+
+export const byLatest = (a: CollectionEntry<"projects">, b: CollectionEntry<"projects">) => a.data.date < b.data.date ? 1 : -1;
