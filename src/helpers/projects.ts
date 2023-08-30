318a4fcd85d56814fb24dc1beb582b5b35e6e441
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2023-08-30 18:45:22 +0200

    Upgrade astro to v3 (#11)
    * upgrade astro to v3
    
    * Fix upgrade issues
    
    * Add page transitions
    
    * Remove transition animation on side-nav
    
    * Revert "Remove transition animation on side-nav"
    
    This reverts commit f7d507ca83707d8a4a47fe5bb6c5dcde98782f07.

--- src/helpers/projects.ts
+++ src/helpers/projects.ts
@@ -1,4 +1,4 @@
-import { CollectionEntry, getCollection } from "astro:content";
+import { type CollectionEntry, getCollection } from "astro:content";
 
 export async function getAllProjects() {
     return await getCollection('projects')
