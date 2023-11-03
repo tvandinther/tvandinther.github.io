8c8c24c66c3ddeaba0ad357db3df9a780a182d94
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2023-11-03 16:07:41 +0100

    Adjust content and layout to support banner image anchoring

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,37 +1,46 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
+import { type Position } from "../types/types.ts";
 
 const blogCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.date(),
-        lastUpdated: z.date().optional(),
-        draft: z.boolean().optional().default(false),
-        author: z.string(),
-        tags: z.array(z.string()),
-        image: z.object({
-            url: z.string(),
-            alt: z.string()
-        }).optional(),
-        categories: z.array(z.string()),
-        featured: z.boolean().optional().default(false),
-    })
-})
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    lastUpdated: z.date().optional(),
+    draft: z.boolean().optional().default(false),
+    author: z.string(),
+    tags: z.array(z.string()),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    categories: z.array(z.string()),
+    featured: z.boolean().optional().default(false),
+  }),
+});
 
 const projectCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.date(),
-        image: z.string().optional(),
-        banner: z.string().optional(),
-        url: z.string().optional(),
-        sourceCodeUrl: z.string().optional(),
-        featured: z.boolean().optional().default(false),
-    })
-})
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    image: z.string().optional(),
+    banner: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+        anchor: z.custom<Position>().optional(),
+      })
+      .optional(),
+    url: z.string().optional(),
+    sourceCodeUrl: z.string().optional(),
+    featured: z.boolean().optional().default(false),
+  }),
+});
 
 export const collections = {
-    'blog': blogCollection,
-    'projects': projectCollection,
-}
+  blog: blogCollection,
+  projects: projectCollection,
+};
