f709205c425ebd35a7858ec0381fe196341c00d0
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2025-01-16 16:06:10 +0100

    Upgrade Astro to v4 (#19)
    * upgrade astro and nodejs
    
    * remove prefetch, update partytown
    
    * bump yarn to 1.22.22
    
    * add nvmrc
    
    * downgrade node to 18.17.1
    
    * update lock file
    
    * Add shell.nix
    
    * Upgrade engine version to node v20
    
    * Update yarn.lock to resolve dependency issues
    
    * Update nvmrc
    
    * Upgrade yarn
    
    * set packageManager
    
    * add new line file terminators

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.mioi.io",
-  integrations: [tailwind(), sitemap(), prefetch(), partytown({ config: { forward: ["dataLayer.push"] } })],
+  integrations: [tailwind(), sitemap(), partytown({ config: { forward: ["dataLayer.push"] } })],
+  prefetch: {
+    defaultStrategy: "viewport",
+    prefetchAll: true,
+  }
 });
